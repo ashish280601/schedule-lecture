069fb9e6927ee1581db4ff767a56c88f7dd49750
unknown <asmehra0628@gmail.com>
2024-07-21 15:10:17 +0530

    added deployment link

--- configAPI.jsx
+++ configAPI.jsx
@@ -1,4 +1,4 @@
-// const apiURL = "https://placement-cell-apis.onrender.com/";
-const apiURL = "http://localhost:8000/api";
+const apiURL = "htthttps://schedule-lecture-apis.onrender.com/api";
+// const apiURL = "http://localhost:8000/api";
 
 export default apiURL;
