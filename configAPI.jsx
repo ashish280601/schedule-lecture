b9e28ebbf75b6443858ecd789dd0c928dd8cc479
unknown <asmehra0628@gmail.com>
2024-07-21 11:19:31 +0530

    Setup my folder structre and dependenscies

---
+++ configAPI.jsx
@@ -0,0 +1,4 @@
+// const apiURL = "https://placement-cell-apis.onrender.com/";
+const apiURL = "http://localhost:8000/api";
+
+export default apiURL;
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css';
+import App from "./App.jsx";
+import "./index.css";
+import store from "../store.jsx";
+import { ToastContainer } from "react-toastify";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+  <>
+    <ToastContainer />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider></>
+
+);
